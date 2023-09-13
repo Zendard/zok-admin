ece5304bb2981dedee884b1f5b91689eb6b2e8ce
Zendard <thijsmuylaert@hotmail.com>
2023-09-13 22:06:13 +0200

    Initial linking

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+import ejs from "ejs";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.render("admin");
+});
+
+app.listen(3000, function () {
+	console.log("Server is running on port 3000 ");
+});
