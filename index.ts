42de97f56661c7dba55792e26d57b953f600bf10
Zendard <thijsmuylaert@hotmail.com>
2023-09-16 15:48:27 +0200

    Add password protection

--- index.ts
+++ index.ts
@@ -7,6 +7,26 @@ if (!Bun.env.MONGODB_URI) {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+	const auth = { login: "marten", password: "studio54" }; // change this
+
+	const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
+	const [login, password] = Buffer.from(b64auth, "base64")
+		.toString()
+		.split(":");
+
+	if (login && password && login === auth.login && password === auth.password) {
+		return next();
+	}
+
+	// Access denied...
+	res.set("WWW-Authenticate", 'Basic realm="401"'); // change this
+	res.status(401).send("Authentication required."); // custom message
+
+	// -----------------------------------------------------------------------
+});
+
 app.get("/", (req, res) => {
 	res.render("admin");
 });
